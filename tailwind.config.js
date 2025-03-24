ff2d2deea661d3284396b71d92bf336cf60cd63e
guimontico <guilherme.montico@townsq.com.br>
2025-03-24 13:54:55 -0300

    Add Tailwind CSS configuration and integrate into the project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,scss}",
+    "./src/app/**/*.{html,ts,scss}",
+    "./src/components/**/*.{html,ts,scss}",
+  ],
+  // Optional: Add your theme customizations
+  theme: {
+    extend: {},
+  },
+  // Optional: Add plugins
+  plugins: [],
+};
